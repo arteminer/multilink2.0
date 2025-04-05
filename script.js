a5ee02d64a6b3645338cfbf69009fbd21d30575c
k3ym <artemurusov2016@gmail.com>
2025-04-05 12:23:45 +0300

    Added files

---
+++ script.js
@@ -0,0 +1,10 @@
+let prank = document.querySelector('.prank');
+let prankap = document.querySelector('.prankap');
+
+prank.addEventListener('click', function(){
+    prankap.style.display = 'block';
+});
+
+prankap.addEventListener('click', function(){
+    prankap.style.display = 'none';
+});
